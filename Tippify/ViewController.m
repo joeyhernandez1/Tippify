9d390b51055d14b68d77acdb045a878eacf1df37
Joey R. Hernandez Perez <joeyrhernandez1@fb.com>
2020-06-23 17:12:00 -0400

    Initial Commit

---
+++ Tippify/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Tippify
+//
+//  Created by Joey R. Hernandez Perez on 6/23/20.
+//  Copyright © 2020 joeyrhernandez1. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
---
+++ TippifyTests/TippifyTests.m
@@ -0,0 +1,37 @@
+//
+//  TippifyTests.m
+//  TippifyTests
+//
+//  Created by Joey R. Hernandez Perez on 6/23/20.
+//  Copyright © 2020 joeyrhernandez1. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TippifyTests : XCTestCase
+
+@end
+
+@implementation TippifyTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
